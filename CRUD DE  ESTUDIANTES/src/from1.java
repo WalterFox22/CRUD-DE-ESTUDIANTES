e67c05d78d2beae4d033b8047001db7482a527f0
WalterFox <john.cobacango@epn.edu.ec>
2024-07-19 12:00:50 -0500

    asd

--- CRUD DE  ESTUDIANTES/src/from1.java
+++ CRUD DE  ESTUDIANTES/src/from1.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 public class from1 {
     public JPanel panel1;
@@ -18,12 +18,32 @@ public class from1 {
             public void actionPerformed(ActionEvent e) {
                 String cedula = ingresoCedula.getText();
 
-                String url = "jdbc:mysql://localhost:3306/estudiates2024";
-                String user = "root";
-                String password = "123456";
+                String url = "jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10720936";
+                String usuario = "sql10720936";
+                String password = "TQeMDi1ru2";
+                Connection conn = null;
+                PreparedStatement pstmt = null;
 
                 String sql = "SELECT * FROM estudiantes WHERE cedula = ?";
-                try (Connection connection = ){
+                try (Connection connection = DriverManager.getConnection(url,usuario,password)){
+                    pstmt = connection.prepareStatement(sql);
+
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    //SETEAMOS LOS VALORES DE LA SENTENCIA PREVIA
+                    pstmt.setInt(1, 9);   // SE LE ASIGNA EL VALOR QUE SE DESEA CAMBIAR DE LA PRIMERA CONDICION SET b1=?
+                    pstmt.setString(2, "0094098135");  // SE LE COLOCA LE CEDULA DEL QUE SE DESEA CAMBIAR
+
+                    //IMPRIMER EL ENUNCIADO
+                    System.out.println(sql);
+                    int result = pstmt.executeUpdate();
+                    System.out.println("se modificaron: " + result + "filas");
+
+
+                } catch (SQLException ex) {
+                    // throw new RuntimeException(ex);
+                    System.out.println(ex.getMessage());
 
                 }
             }
